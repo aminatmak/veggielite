022085cc62245b6d655427efc6d332ffefca7191
Amina Makhusseyeva <amina.makhusseyeva@gmail.com>
2021-11-14 22:39:15 +0400

    Controllers added

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,3 @@
+class ProductsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :home, :index, :show ]
+end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -6,8 +6,8 @@ class Shop < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :phone_number, presence: true
   validates :address, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
 end
