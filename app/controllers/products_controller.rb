920d46dca30b1c70174213dd01eabd285fc78fd3
Amina Makhusseyeva <amina.makhusseyeva@gmail.com>
2021-11-15 17:44:50 +0400

    Products home and index page are done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,27 @@
 class ProductsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :index, :show ]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def create
+    @product = Product.create(product_params)
+    if @product.save
+      redirect_to @product, notice: 'Product was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :category, :quantity)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :products, only: [:index, :show, :new] do
+    resources :orders, only: [:create]
+  end
+
+  resources :orders, only: [:destroy]
+  get '/my_orders', to: 'my_orders#my_orders'
 end
