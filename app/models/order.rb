9c8178eec5518d683bf151ad29b0aa6407b151ca
Jane Braimah <jane.noelab@gmail.com>
2021-11-17 18:07:00 +0400

    MyOrders template.

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,5 @@ class Order < ApplicationRecord
   belongs_to :shop
   belongs_to :user
   has_many :order_product_relationships
+  has_many :products, through: :order_product_relationships
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :my_orders, only: :index
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :products, only: [:index, :show, :new] do
@@ -10,4 +11,5 @@ Rails.application.routes.draw do
   resources :shops, only: [:index, :show]
   resources :orders, only: [:destroy]
   get '/my_orders', to: 'my_orders#my_orders'
+  get '/profile', to: 'profiles#profile'
 end
