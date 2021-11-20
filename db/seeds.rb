fd2063552f5c0623b40f0fe75500d3632d5f1f41
Amina Makhusseyeva <amina.makhusseyeva@gmail.com>
2021-11-20 23:26:40 +0400

    Added 8 shops and 5 products

--- db/seeds.rb
+++ db/seeds.rb
@@ -60,13 +60,13 @@ shop1.save
 p shop1
 
 shop2 = Shop.create(
-  name: 'iHerb',
-  address: 'California',
-  phone_number: '012354536987',
-  email: 'help@iherb.com'
+  name: 'Organic & Real',
+  address: 'Mohideen Paper Products Industry - Warehouse #46 - 46 16b Street - Al Quoz Industrial Area 4 - Dubai',
+  phone_number: '00971588403164',
+  email: 'care@organicandreal.com'
 )
-file = URI.open('https://res.cloudinary.com/le-wagon-dubai-fatymah/image/upload/v1637142900/vegl/shop/1024x512-1624016160208_gifmhb.webp')
-shop2.photos.attach(io: file, filename: 'iherb', content_type: 'image/jpg')
+file = URI.open('https://res.cloudinary.com/diepahdjf/image/upload/v1637434062/store_653_xwqyhy.jpg')
+shop2.photos.attach(io: file, filename: 'organic&real', content_type: 'image/jpg')
 shop2.save
 p shop2
 
@@ -103,12 +103,99 @@ shop5.photos.attach(io: file, filename: 'lulus', content_type: 'image/jpg')
 shop5.save
 p shop5
 
+shop6 = Shop.create(
+  name: "Fresh Del Monte Produce Inc.",
+  address: 'Street 14 & 17, Central Fruit and Vegetable Market Area, Awir',
+  phone_number: '0097143333801',
+  email: 'info-uae@freshdelmonte.com'
+)
+file = URI.open('https://res.cloudinary.com/diepahdjf/image/upload/v1637431459/1200px-Del_Monte_logo.svg_sjtzcb.png')
+shop6.photos.attach(io: file, filename: 'delmonte', content_type: 'image/jpg')
+shop6.save
+p shop6
+
+shop7 = Shop.create(
+  name: "Union Coop",
+  address: 'Dubai, Al Warqa-3 , the Tripoli Street',
+  phone_number: '00971458008889',
+  email: 'info@unioncoop.ae'
+)
+file = URI.open('https://res.cloudinary.com/diepahdjf/image/upload/v1637431532/FINAL-APPROVAL-UNION-COOP-LOGO_u7ad9b.png')
+shop7.photos.attach(io: file, filename: 'unioncoop', content_type: 'image/jpg')
+shop7.save
+p shop7
+
+shop8 = Shop.create(
+  name: "QualityFood",
+  address: 'Street 14 & 17, Central Fruit and Vegetable Market Area, Awir Dubai',
+  phone_number: '00971524353400',
+  email: 'customer.service@qualityfood.ae'
+)
+file = URI.open('https://s3-eu-west-1.amazonaws.com/tpd/logos/5a6a60381ffc8700012341e1/0x0.png')
+shop8.photos.attach(io: file, filename: 'qualityfood', content_type: 'image/jpg')
+shop8.save
+p shop8
+
+shop9 = Shop.create(
+  name: "Be Vegan",
+  address: 'Fahidi 7 - Al Esbij St - Al Fahidi - Dubai',
+  phone_number: '00971586975997',
+  email: 'hello@bevegan.ae'
+)
+file = URI.open('https://res.cloudinary.com/diepahdjf/image/upload/v1637433454/bevegan-new-logo_105x_2x_bgw53b.png')
+shop9.photos.attach(io: file, filename: 'west zone', content_type: 'image/jpg')
+shop9.save
+p shop9
+
+shop10 = Shop.create(
+  name: "BiOrganic",
+  address: '4 B St - Al Quoz - Dubai',
+  phone_number: '0097143372199',
+  email: 'admin@preciousfood.com'
+)
+file = URI.open('https://res.cloudinary.com/diepahdjf/image/upload/v1637432177/Biorganic-logo_frgmyg.png')
+shop10.photos.attach(io: file, filename: 'biorganic', content_type: 'image/jpg')
+shop10.save
+p shop10
+
+shop11 = Shop.create(
+  name: "Emirates Bio Farm",
+  address: '8th St - Abu Dhabi',
+  phone_number: '0097137838422',
+  email: 'info@emiratesbiofarm.com'
+)
+file = URI.open('https://res.cloudinary.com/diepahdjf/image/upload/v1637432337/yvnEgU_u_400x400_zmqcrl.jpg')
+shop11.photos.attach(io: file, filename: 'emirates bio farm', content_type: 'image/jpg')
+shop11.save
+p shop11
+
+shop12 = Shop.create(
+  name: "GoGoGuy",
+  address: 'Boutique villa 1, Dubai Media City - Dubai',
+  phone_number: '009714 427 2466',
+  email: 'go@gogoguy.com'
+)
+file = URI.open('https://res.cloudinary.com/diepahdjf/image/upload/v1637432410/tvugqtpzkkaqx3ernmnr.png')
+shop12.photos.attach(io: file, filename: 'gogoguy', content_type: 'image/jpg')
+shop12.save
+p shop12
+
+shop13 = Shop.create(
+  name: "Maison Duffour",
+  address: 'Warehouse 7 Street 10A - Al Quoz Industrial Area 3 - Dubai',
+  phone_number: '00971509551520',
+  email: 'contact@maisonduffour.com'
+)
+file = URI.open('https://res.cloudinary.com/diepahdjf/image/upload/v1637434401/CROPPED_LOGO-JAUNE-RVB_1_1b6acfc3-5de5-4305-b684-54d80c98938b_1200x1200_evw7jt.jpg')
+shop13.photos.attach(io: file, filename: 'maison duffour', content_type: 'image/jpg')
+shop13.save
+p shop13
+
 puts "Done with the shops"
 p "-" * 20
 puts "Creating the products"
 
 product1 = Product.create(
-  sku: 'red-mill-pancake',
   name: 'Red Mill Pancake',
   description: 'Heat a lightly greased griddle or pan over medium-high heat. Mix all ingredients together in a bowl until just combined and then pour 1/4-cup portions onto the hot griddle or pan. Cook until bubbly, then flip and let cook until done, about 2-3 minutes per side. Serve immediately. Makes about 8 pancakes.',
   price_cents: 2350,
@@ -125,7 +212,6 @@ product1.save
 p product1
 
 product2 = Product.create(
-  sku: 'natural-bio-rice-drink',
   name: 'The Bridge Natural Bio Rice Drink',
   description: 'The Bridge rice milk is a completely vegetable drink, issued from organically grown Italian rice, of pleasant taste and excellent digestibility also for babies.',
   price: 13,
@@ -142,7 +228,6 @@ product2.save
 p product2
 
 product3 = Product.create(
-  sku: 'sugar-free-green-cubes',
   name: 'Sugar Free Green Cubes',
   description: "It is 100 percent natural and is made from Stevia leaves. It's the perfect natural sweetener for beverages and desserts that you can share with your family including kids.",
   price: 13,
@@ -159,7 +244,6 @@ product3.save
 p product3
 
 product4 = Product.create(
-  sku: 'sheese-vegan-mozzarella-slices',
   name: 'Sheese Vegan Mozzarella Slices',
   description: "Bute Islands's Mature Cheddar style Sheese is a 100% soya and dairy free cheese alternative made with coconut oil. It's free from lactose, gluten and produced using animal free process.",
   price_cents: 24.99,
@@ -176,7 +260,6 @@ product4.save
 p product4
 
 product5 = Product.create(
-  sku: 'mango',
   name: 'Mango',
   description: 'A good ol mango.',
   price_cents: 4500,
@@ -193,7 +276,6 @@ product5.save
 p product5
 
 product6 = Product.create(
-  sku: 'salmon-burgers-herbs',
   name: 'Fresh Express Salmon Burgers with Herbs',
   description: "Salmon Burgers with Herbs are produced by Fresh Express Foodstuff LLC, an established brand and industry leader in live, fresh and frozen seafood.",
   price: 20,
@@ -201,7 +283,7 @@ product6 = Product.create(
   quantity: 1,
   supplier_country: 'UAE',
   calories: 300,
-  shop: shop1,
+  shop: shop6,
   macros: { protein: 20, carbs: 5, fat: 13 }
 )
 file = URI.open('https://res.cloudinary.com/le-wagon-dubai-fatymah/image/upload/v1637142922/vegl/product/1194875_main_huo4aj.jpg')
@@ -210,7 +292,6 @@ product6.save
 p product6
 
 product7 = Product.create(
-  sku: 'organic-range-eggs',
   name: 'Organic Larder Organic Free Range Eggs',
   description: "The Organic Larder Organic Eggs are loaded with protein and vitamins for your daily requirement. These are free-range and are available in a crate of 10 eggs.",
   price_cents: 2230,
@@ -227,7 +308,6 @@ product7.save
 p product7
 
 product8 = Product.create(
-  sku: 'nestle-honey-flakes',
   name: 'Nestle Go Free Honey Flakes',
   description: 'Delicious flakes of golden corn with a touch of honey.',
   price: 21,
@@ -244,7 +324,6 @@ product8.save
 p product8
 
 product9 = Product.create(
-  sku: 'symega-vanilla-powder',
   name: 'Symega Sugar Free Vanilla Powder',
   description: "Sugar free vanilla powder for all your sweet treats!",
   price_cents: 4800,
@@ -261,7 +340,6 @@ product9.save
 p product9
 
 product10 = Product.create(
-  sku: 'natures-pat-organic-granola',
   name: "Nature's Path, Love Crunch, Premium Organic Granola, Dark Chocolate & Red Berries",
   description: 'Made with Organic Coconut & Cocoa',
   price: 16,
@@ -278,7 +356,6 @@ product10.save
 p product10
 
 product11 = Product.create(
-  sku: 'creamy-sheese-original',
   name: 'Creamy Sheese Original',
   description: 'Our Original Creamy Sheese is a versatile dairy free cream cheese alternative. It can be used in exactly the same way as dairy cream cheese, sweet or savoury, except it’s certified vegan and kosher and made with coconut oil.',
   price: 25,
@@ -295,7 +372,6 @@ product11.save
 p product11
 
 product12 = Product.create(
-  sku: 'ina-paarmans-kitchen-fish-spice-seasoning',
   name: "Ina Paarman's Kitchen Fish Spice Seasoning",
   description: 'This is an all-in-one rub with salt added. It is a blend of seasoned flour and breadcrumbs with lemon and herbs. The flour and breadcrumbs promote browning of the fish and prevent moisture loss.',
   price: 17,
@@ -312,7 +388,6 @@ product12.save
 p product12
 
 product13 = Product.create(
-  sku: 'wilderness-poets-raw-pistachio-butter',
   name: 'Wilderness Poets Raw Pistachio Butter',
   description: 'Wilderness Poets Pistachio Butter is a delicious, creamy, naturally glowing green nut butter, made from fresh, Organic, Raw Pistachios.',
   price: 88,
@@ -329,7 +404,6 @@ product13.save
 p product13
 
 product14 = Product.create(
-  sku: 'nectar-mini-peanut-butter-cups',
   name: 'Nectar Mini Peanut Butter Cups',
   description: 'Nectar is a unique homegrown concept created in 2013 in the heart of Abu Dhabi that offers functional and nourishing foods and drinks that go beyond basic nutrition. We use the best seasonal, local, and organic ingredients to carefully craft replenishing juices and smoothies, grain- and refined sugar-free snacks and treats, and handcrafted salads with superfood ingredient.',
   price: 30,
@@ -346,7 +420,6 @@ product14.save
 p product14
 
 product15 = Product.create(
-  sku: 'gluten-free-unsweetened-cacao-powder',
   name: 'Bites Of Delight Gluten-Free Vegan Unsweetened Cacao Powder',
   description: 'This product could be safely consumed by celiac disease patients and people with gluten intolerance as it contains less than 20 ppm, a standard established and enforced by the FDA. ',
   price: 28,
@@ -363,7 +436,6 @@ product15.save
 p product15
 
 product16 = Product.create(
-  sku: 'almond',
   name: 'Almonds',
   description: 'Almonds are a nutritious, portable, low-carbohydrate food that can be used in cooking or eaten raw as is. Almonds can be purchased, salted, unsalted, raw, or roasted. They can be turned into almond butter or ground as almond meal.',
   price_cents: 1874,
@@ -380,7 +452,6 @@ product16.save
 p product16
 
 product17 = Product.create(
-  sku: 'nectar-grain-free-herb-crackers',
   name: 'Nectar Grain-Free Herb Crackers',
   description: 'Nectar Grain-Free Herb Crackers (45g)',
   price: 20,
@@ -397,7 +468,6 @@ product17.save
 p product17
 
 product18 = Product.create(
-  sku: 'organik-mong-fruit',
   name: 'NOW Foods Real Food Organic Monk Fruit 1 to 1 Sugar Replacement',
   description: "Discover and enjoy the enlightened sweetener! Monk fruit is nature's perfect substitute for sugar. You will be pleasantly surprised by how great it tastes, because it is deliciously sweet without any bitter aftertaste like that of artificial sweeteners. At zero calories, monk fruit is perfect for keto and other no-sugar diets.",
   price: 59,
@@ -414,7 +484,6 @@ product18.save
 p product18
 
 product19 = Product.create(
-  sku: 'ceylon-naturals-organic-coconut-cream',
   name: 'Ceylon Naturals Organic Coconut Cream',
   description: 'Ceylon Naturals Organic Coconut Cream is produced from the finest coconut meat from the best coconut harvests organically grown in Sri Lanka.',
   price_cents: 8950,
@@ -431,7 +500,6 @@ product19.save
 p product19
 
 product20 = Product.create(
-  sku: 'fullgreen-riced-cauliflower',
   name: 'FullGreen Riced Cauliflower With Tomato, Garlic, & Herbs',
   description: "Fullgreen's Rice Cauliflower with Tomato, Garlic, & Herbs is the ideal low-carb side to meals that call for a rich tomato flavor.",
   price: 19,
@@ -442,7 +510,88 @@ product20 = Product.create(
   shop: shop5,
   macros: { protein: 1.5, carbs: 6, fat: 1 }
 )
-file = URI.open('https://res.cloudinary.com/le-wagon-dubai-fatymah/image/upload/v1637142921/vegl/product/1.Riced_UK_Tomato_Front_1_2400x2400-600x600-removebg-preview_1_540x_dq4gvj.png')
+file = URI.open('https://res.cloudinary.com/diepahdjf/image/upload/v1637434613/SKINNYGENIEBreadLoafBrownMultigrain400g_Gluten-free_Vegan_1000x_fwybxt.jpg')
 product20.photo.attach(io: file, filename: 'riced', content_type: 'image/jpg')
 product20.save
 p product20
+
+product21 = Product.create(
+  name: 'Skinny Genie Brown Loaf Bread Multigrain',
+  description: "This Gluten Free and Vegan Brown Multigrain Loaf Bread is manufactured in a 100% Gluten Free Environment and Certified by Association of European Coeliac Society, as well as certified by Vegan Australia! Ingredients: Potato Starch, Maize Starch, Rice flour, thickener E415, E412, E464 enymes, linseeds, sunflower seeds, soya protein concentrate, sugar, psyllium, dried sourdough, caramel, ground roasted lentils, yeast extract, maltodextrin, salt, acidity regulaotr E 262, Enyzme, alpha-amylase, sunflower oil, pumpkin seeds, sesame seeds. ",
+  price: 24,
+  categories: ['Gluten free', 'Diary free', 'Sugar free'],
+  quantity: 1,
+  supplier_country: 'Australia',
+  calories: 769,
+  shop: shop13,
+  macros: { protein: 26, carbs: 123, fat: 15 }
+)
+file = URI.open('https://res.cloudinary.com/le-wagon-dubai-fatymah/image/upload/v1637142921/vegl/product/1.Riced_UK_Tomato_Front_1_2400x2400-600x600-removebg-preview_1_540x_dq4gvj.png')
+product21.photo.attach(io: file, filename: 'bread', content_type: 'image/jpg')
+product21.save
+p product21
+
+product22 = Product.create(
+  name: 'Organic Atlantic Sea Spaghetti - Dried Sea Vegetable',
+  description: "Due to the nature of this product, this pack may contain a trace amount of fish, molluscs and/or crustaceans. Guidance from food safety authorities suggests limiting consumption of Hijiki to 5g dry weight per week. Sea vegs are naturally rich in iodine. People sensitive to the effects of iodine are advised to seek medical advice before consuming this product. Product weight: 30g. Ingredients: Dried Hijiki sea vegetable (Hijikia fusiforme). Its colour contrasts beautifully with other vibrant ingredients and works wonderfully well when combined with grains and other vegetables.",
+  price: 50,
+  categories: ['Pescetarian', 'Vegan'],
+  quantity: 1,
+  supplier_country: 'UK',
+  calories: 15,
+  shop: shop12,
+  macros: { protein: 0.5, carbs: 1, fat: 1 }
+)
+file = URI.open('https://res.cloudinary.com/diepahdjf/image/upload/v1637435286/SVG0901.025A-Organic-Atlantic-Sea-Spaghetti_a2dee571-c56f-445f-9849-3277c47bd43a_2000x_b6zrm7.jpg')
+product22.photo.attach(io: file, filename: 'sea spaghetti', content_type: 'image/jpg')
+product22.save
+p product22
+
+product23 = Product.create(
+  name: 'CLEARSPRING Organic Instant Miso Soup with Sea Vegetables, 4 x 10g - 4 Single Servings',
+  description: "Miso is a flavourful Japanese soya food that makes a rich, satisfying soup. This luxurious tasting organic miso soup is made with two varieties of miso and two kinds of sea vegetables. Ingredients: Freeze-dried brown rice miso* (83%) (whole soya beans*, brown rice*, sea salt), freeze-dried sweet white miso* (11%) (whole soya beans*, rice*, sea salt), dried wakame (3%), dried green onions*, kombu powder (0.3%). *organically grown",
+  price: 28,
+  categories: ['Organic', 'Pescetarian'],
+  quantity: 1,
+  supplier_country: 'UK',
+  calories: 285,
+  shop: shop11,
+  macros: { protein: 21, carbs: 25, fat: 8,6 }
+)
+file = URI.open('https://res.cloudinary.com/diepahdjf/image/upload/v1637435613/image_1200x_59fd0d52-231d-441e-a4c3-b1e899daa3af_1000x_yemie9.jpg')
+product23.photo.attach(io: file, filename: 'miso', content_type: 'image/jpg')
+product23.save
+p product23
+
+product24 = Product.create(
+  name: 'CLEARSPRING Organic Japanese Tofu Silken & Smooth',
+  description: "Clearspring's award-winning Tofu is a delicious organic, high-protein and low-fat food that tastes as good as the fresh tofu made in Japan. It is ready to enjoy straight from the pack and is extremely versatile - perfect for use in numerous savoury and sweet dishes.",
+  price: 15,
+  categories: ['Vegan', 'Vegetarian'],
+  quantity: 1,
+  supplier_country: 'UK',
+  calories: 228,
+  shop: shop10,
+  macros: { protein: 24, carbs: 5.7, fat: 14.4 }
+)
+file = URI.open('https://res.cloudinary.com/diepahdjf/image/upload/v1637435852/ClearspringTofuIMG_0805_800x_997f8cd6-032c-42b4-af2d-04adfc70f9d9_800x_mvtt4s.jpg')
+product24.photo.attach(io: file, filename: 'tofu', content_type: 'image/jpg')
+product24.save
+p product24
+
+product25 = Product.create(
+  name: 'ORGANIC LARDER Pesto Vegan With Tofu',
+  description: "Our Italian Family Has Been Making Pesto For Generations And This Is Our Organic Tomato Recipe Will Add Pizzazz To Your Italian Meals.
+We Have Been Making Pesto For Over 75 Years And Our Organic Pesto Is Made To Strict Organic Standards And Is Suitable For Vegetarians.",
+  price: 44,
+  categories: ['Gluten free', 'Vegetarian', 'Organic'],
+  quantity: 1,
+  supplier_country: 'Italy',
+  calories: 642,
+  shop: shop9,
+  macros: { protein: 6.3, carbs: 12.2, fat: 62.7 }
+)
+file = URI.open('https://res.cloudinary.com/diepahdjf/image/upload/v1637436354/2632_800x_l7s8jr.jpg')
+product25.photo.attach(io: file, filename: 'pesto', content_type: 'image/jpg')
+product25.save
+p product25
