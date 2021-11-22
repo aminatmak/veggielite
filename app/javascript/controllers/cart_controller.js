f3701111f51ea6838a3cf11b6824c7af1af14fc3
Amina Makhusseyeva <amina.makhusseyeva@gmail.com>
2021-11-22 11:28:16 +0400

    add and remove multiple items to the cart

--- app/javascript/controllers/cart_controller.js
+++ app/javascript/controllers/cart_controller.js
@@ -3,24 +3,42 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   static targets = ['add', 'remove', 'links'];
 
+
+  initialize() {
+    // 1. get the basket array
+    const basket = document.querySelector('basket')
+    // 2. how many times do we find the id in the array -> [ [id, occurence],[4,2], [7,1] ]
+    // 3. iterate through the array
+    // 4. for each id -> target the product-id
+    // 5. update the innertext of target with occurence
+  }
+
+  connect() {
+
+  }
+
+
   async addProduct(event) {
     event.preventDefault();
     event.stopPropagation();
 
-    const increaseNumber = document.querySelector('number').value,
-    const increaseNumber = $(this).parents('.cart').find('.quantity').val();
-    const value = parseInt(increaseNumber, 10);
-    value = isNaN(value) ? 0 : value;
-    if (value < 10) {
-      value++;
-      $(this).parents('.cart').find('.quantity').val(value);
-    }
+    // 1. Get the index of the box that needs increasing
+    const target = event.currentTarget.dataset.index
+    // console.log(target)
+
+    // 2. select the box
+    const numberField = document.getElementById(target)
+    // console.log(numberField)
+
+    // 3. increment the value of the box
+    numberField.innerText = parseInt(numberField.innerText) + 1
 
     // select the counter
     const counter = document.querySelector('#counter')
-    console.log(counter)
+    // console.log(counter)
     // increase the value by 1
-    counter.innerHTML = parseInt(counter.innerText) + 1
+    counter.innerText = parseInt(counter.innerText) + 1
+
     const url = this.addTarget.href;
     const response = await fetch(url, {
       method: 'POST',
@@ -29,31 +47,25 @@ export default class extends Controller {
       }
     })
     const parsedResponse = await response.text();
-    console.log(parsedResponse)
+    // console.log(parsedResponse)
 
-
-    // 1. increase the value of the field in between the two links by 1
-    //  TODO
-
-    // this.linksTarget.outerHTML = parsedResponse;
   }
 
   async removeProduct(event) {
     event.preventDefault();
     event.stopPropagation();
 
-    const decrementNumber = document.querySelector('number').value,
-    const decrementNumber = $(this).parents('.cart').find('.quantity').val();
-    const value = parseInt(decrementNumber, 10);
-    value = isNaN(value) ? 0 : value;
-    if (value > 1) {
-      value--;
-      $(this).parents('.cart').find('.quantity').val(value);
-    }
+    const target = event.currentTarget.dataset.index
+    // console.log(target)
+    const numberField = document.getElementById(target)
+    console.log(numberField)
+    numberField.innerText = parseInt(numberField.innerText) - 1
+
     // select the counter
     // decrease the value by 1
     const counter = document.querySelector('#counter')
-    counter.innerHTML = parseInt(counter.innerText) - 1
+    counter.innerText = parseInt(counter.innerText) - 1
+
     const url = this.removeTarget.href;
     const response = await fetch(url, {
       method: 'DELETE',
