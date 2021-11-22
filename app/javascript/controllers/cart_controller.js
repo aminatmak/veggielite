1965a57957cf2d0b34342ba7bb082b25ccfd32fd
Amina Makhusseyeva <amina.makhusseyeva@gmail.com>
2021-11-22 12:43:35 +0400

    cart qunatity update

--- app/javascript/controllers/cart_controller.js
+++ app/javascript/controllers/cart_controller.js
@@ -1,17 +1,31 @@
 import { Controller } from "stimulus";
 
+let basket;
+
 export default class extends Controller {
   static targets = ['add', 'remove', 'links'];
 
 
   initialize() {
     // 1. get the basket array
-    const basket = document.querySelector('basket')
-    // 2. how many times do we find the id in the array -> [ [id, occurence],[4,2], [7,1] ]
+    basket = JSON.parse(this.element.dataset.basket)
 
-    // 3. iterate through the array
-    // 4. for each id -> target the product-id
-    // 5. update the innertext of target with occurence
+    // 2. how many times do we find the id in the array -> [ [id, occurence],[4,2], [7,1] ]
+    const counts = {}
+    basket.forEach(number => {
+      counts[number] = counts[number] ? counts[number] + 1 : 1;
+    } )
+
+    // 3. iterate through the hash
+    Object.keys(counts).forEach( id => {
+      // 4. for each id -> target the product-id
+      // 5. update the innertext of target with occurence
+      const product = parseInt(this.element.dataset.product)
+
+      if (parseInt(id) === product) {
+         this.element.querySelector('span').innerText = counts[id]
+      }
+    })
   }
 
   connect() {
