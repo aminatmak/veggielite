df7901938663d96f1d6fd8ef44343e9d730ac975
Amina <71384858+aminatmak@users.noreply.github.com>
2022-07-26 15:12:59 +0400

    adding spec for orders

--- spec/requests/orders_spec.rb
+++ spec/requests/orders_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Orders", type: :request do
-  describe "GET /orders" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /orders' do
+    it 'should list all orders' do
+      get orders_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'POST /order' do
+    let(:product) { create :product }
+    let(:user) { create :user }
+    it 'should return status ok' do
+      expect(response).to have_http_status(200)
+    end
   end
 end
